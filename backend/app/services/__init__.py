7236afef73bda3f85dccd147695d5e73cae63135
umohelisha0-bit <umohelisha0@gmail.com>
2026-07-05 06:21:07 -0700

    Create services package

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,7 @@
+"""Services package"""
+from .ai_service import AIService
+from .speech_service import SpeechService
+from .avatar_service import AvatarService
+from .emotion_service import EmotionService
+
+__all__ = ["AIService", "SpeechService", "AvatarService", "EmotionService"]
